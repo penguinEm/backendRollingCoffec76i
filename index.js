6762b3445bee752f09366f6d8b8aa6ae72fbd208
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-05 18:10:34 -0300

    Configuramos un puerto para el proyecto de backend

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /* importo el expres para no usar el required - configuro en el package json que el type se4a module */
-import  express  from "express"
-
-console.log("hola mundo")
-console.log("hola mundo2")
-console.log("hola mundo3")
-
-
+import express from "express";
 
 /* 1 - Configurar un PUERTO */
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+    console.log("Estoy en el puerto " + app.get("port"))
+})
 
 /* 2 - Configurar los MIDDLEWARES - funciones que le dan "habilidades al backend" */
 
