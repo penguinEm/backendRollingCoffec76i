e87254fc8d4908052e19b98cd275273463a707a7
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-05 17:24:43 -0300

    Instalamos express, y habilitamos los modulos

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/* importo el expres para no usar el required - configuro en el package json que el type se4a module */
+import  express  from "express"
+
 console.log("hola mundo")
+
+/* 1 - Configurar un PUERTO */
+
+/* 2 - Configurar los MIDDLEWARES - funciones que le dan "habilidades al backend" */
+
+/* 3 - Configurar las RUTAS */
