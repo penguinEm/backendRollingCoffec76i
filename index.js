d949146650baa1a6be9312a00db1db3d294b1364
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-07 17:01:34 -0300

    Configuramos un archivo eztatico en la ruta principal del proyecto

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 /* importo el expres para no usar el required - configuro en el package json que el type se4a module */
 import express from "express";
 
-/* 1 - Configurar un PUERTO */
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+/* imports para construir las rutas hacia el index (configuracion D de los middlewares) */
+import path from "path";
+import { fileURLToPath } from "url";
+
+//! 1 - Configurar un PUERTO
 const app = express();
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
-    console.log("Estoy en el puerto " + app.get("port"))
-})
+  console.log("Estoy en el puerto " + app.get("port"));
+});
 
-/* 2 - Configurar los MIDDLEWARES - funciones que le dan "habilidades al backend" */
+//! 2 - Configurar los MIDDLEWARES - funciones que le dan "habilidades al backend"
+//a - el primero es importar dotenv para procesar variables de entorno
+//b - cors: permite obtener configuraciones remotas
+app.use(cors());
+//c - morgan: obtenemos informacion extra en la terminal (que tipo de solicitud se realizo y que respuesta damos)
+app.use(morgan("dev"));
+//d - express: interpreta el formato json
+app.use(express.json());
+//e - express: interpreta como acceder a los datos del body del request
+app.use(express.urlencoded({ extended: true }));
+//f - express: interpretar la ubicacion del index.html desde public
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, "/public")));
 
-/* 3 - Configurar las RUTAS */
+
+//! 3 - Configurar las RUTAS
+//http://localhost:4001/
+app.get("/", (req, res) => {
+  /* Agregar toda la logica de nuestro backend */
+  console.log("Procesando una solicitud get");
+  res.send("Respuesta del backend RollingCoffe");
+});
