91c8f828b3e3717b26315ba98c2e242586a4169a
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-12 17:31:45 -0300

    creamos el schema de producto

---
+++ src/database/models/producto.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 10000,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor) => {
+        const pattern = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/;
+        return pattern.test(valor);
+      },
+      message: (dato) => `${dato.value} no es una URL de imagen válida`,
+    },
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Panaderia", "Cafeteria", "Reposteria"],
+  },
+  descripcionBreve: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 250,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLength: 30,
+    maxLength: 1000,
+  },
+});
