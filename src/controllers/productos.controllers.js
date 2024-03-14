436552705f365f20bfc75d35ca634083d510c087
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-14 17:18:04 -0300

    Agregamos la funcion para obtener 1 producto por id mediante parametro

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -2,15 +2,38 @@
 import Producto from "../database/models/producto.js";
 
 //! GET de todos los productos
-export const listarProductos = async(req, res) => {
+export const listarProductos = async (req, res) => {
   try {
-    const productos = await Producto.find() //al find le puedo agregar filtros separado por comas
-    res.status(200).json(productos)
+    const productos = await Producto.find(); //al find le puedo agregar filtros separado por comas
+    res.status(200).json(productos);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500)({
-      mensaje: "Error al listar los productos"
-    })
+      mensaje: "Error al listar los productos",
+    });
+  }
+};
+
+//! GET de 1 producto
+export const obtenerProducto = async (req, res) => {
+  try {
+    //extraer el id de la ruta
+    console.log(req.params.id)
+    //solicidar a la bd buscar ese producto
+    const productoBuscado = await Producto.findById(req.params.id)
+    //pregutnar si no se encontro el producto
+    if (productoBuscado === null){
+      return res.status(404).json({
+        mensaje: "Producto con el id enviado no existe"
+      })
+    }
+    //enviar respuesta
+    res.status(200).json(productoBuscado)
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      mensaje: "No se pudo encontrar el producto solicitado",
+    });
   }
 };
 
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 /* Instancio el enrutador de express, el cual me permitira crear rutas */
 import { Router } from "express";
-import { crearProductoDb, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProductoDb, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 const router = Router();
 
 //! Como crear rutas.
 router.route("/productos").get(listarProductos).post(crearProductoDb)
+router.route("/productos/:id").get(obtenerProducto)
 
 export default router;
